d4abdaba8561b91db995802e51401932cdd1ce35
Ajay Sathish (31st) <ajay.sathish@thirtyfirstunion.com>
2024-11-20 18:45:11 -0700

    Initial commit of files, plus added base character class

---
+++ Source/Mechanics/Private/Player/WarPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/WarPlayerState.h"
+
+AWarPlayerState::AWarPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Mechanics/Public/Player/WarPlayerState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "WarPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHANICS_API AWarPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AWarPlayerState();
+	
+};
