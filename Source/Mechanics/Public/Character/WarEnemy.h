389c24e297f8d7a6b8950cb2789eed9915c85c38
Ajay Sathish (31st) <ajay.sathish@thirtyfirstunion.com>
2024-11-20 19:05:51 -0700

    Added character and AI character class

---
+++ Source/Mechanics/Public/Character/WarEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WarCharacterBase.h"
+#include "WarEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHANICS_API AWarEnemy : public AWarCharacterBase
+{
+	GENERATED_BODY()
+	
+};
